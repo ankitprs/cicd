436f571d93d7cacbfd62e6b261eb1bfc7c7b1474
ankitprasad <ankitpr2001@gmail.com>
2024-03-02 01:40:30 +0530

    version 1

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express();
+
+
+
+app.get('/', function(req, res){
+	res.send("server is running on 8080")
+})
+
+
+app.listen(8080, function(req, res) {
+	console.log(`server is running on 8080`)
+})
