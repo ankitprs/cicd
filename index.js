d28f7218c305ae3dcee87e3584cb49dabb1433ed
ankitprasad <ankitpr2001@gmail.com>
2024-03-07 03:45:23 +0530

    18

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const express = require("express")
 const app = express();
 
 
+app.get('/hi', function (req, res) {
+  res.send("Ankit's server hi")
+})
+
 app.get('/', function (req, res) {
   res.send("server is running on 8080")
 })
 
- 
+
 app.listen(8080, function (req, res) {
   console.log(`server is running on 8080`)
 })
